812a91cb3b0596b94d76cdda45719a5ee66088e2
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 08:24:33 +0200

    created a new @Post request method returning its Body. I can also grab a specific property from it passing its name into the @Body() decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +10,16 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id) {
     return `This action returns the coffee with id ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    // JSON for insomnia or whatever
+    // {
+    //   "name": "Old Florida Roast",
+    //   "brand": "Buddy Brew"
+    // }
+    return body;
+    // return `This action creates a coffee`;
   }
 }
