f531b0f397c68dd62f76c7e1d4b7d852e7a3133b
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 09:53:44 +0200

    updated / simplified the findOne() method error handling using the NestJs buildIn NotFoundException.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -19,10 +24,7 @@ export class CoffeesService {
   findOne(id: string) {
     const coffee = this.coffees.find((item) => item.id === +id);
     if (!coffee) {
-      throw new HttpException(
-        `Coffee with id ${id} not found`,
-        HttpStatus.NOT_FOUND,
-      );
+      throw new NotFoundException(`Coffee with id ${id} not found`);
     }
     return coffee;
   }
