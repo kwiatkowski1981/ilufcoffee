ec43e912c3656bb79dfa9adcc8084c076c7b803d
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 09:24:44 +0200

    added a single Entity and defined coffees array as the Coffee typ.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees = [];
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
 }
