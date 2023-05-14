7f79474b5e7dbcee8764db8d97b918bea02eb678
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 20:10:15 +0200

    added specific decorators in the coffee Entity such as @PrimaryGeneratedColumn(), @Column()

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,9 +1,13 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+  @Column('json', { nullable: true })
   flavors: string[];
 }
