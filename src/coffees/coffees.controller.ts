62b0cdc15a07a4ff47cbea93f99281944a9d9937
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 09:38:39 +0200

    updated my controller and its service, changed the methods that the controller use its service now.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,42 +2,44 @@ import {
   Body,
   Controller,
   Delete,
-  Get,
+  Get, HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
-  Query,
-} from '@nestjs/common';
+  Query
+} from "@nestjs/common";
 import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
+    // const { limit, offset } = paginationQuery;
     // route to hit: localhost:3000/coffees?limit=10&offset=20
-    return `This action returns all coffees. Limit: ${limit}, Offset: ${offset}`;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
-    // return `This action creates a coffee`;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
