278f06c2923e201d71c1567ace0e2b864ee9ee56
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-14 10:47:06 +0200

    deleted unused folder, moved coffees DTOs to the right folder and updated imports

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,8 +11,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
-import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/create-coffee.dto/update-coffee.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
