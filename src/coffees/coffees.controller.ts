e348493c0b493a12ed88e7a4cb323ebe6539f7c3
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-15 11:45:47 +0200

    added pagination to my queries

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,6 +14,7 @@ import {
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -21,10 +22,10 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
     // const { limit, offset } = paginationQuery;
     // route to hit: localhost:3000/coffees?limit=10&offset=20
-    return this.coffeesService.findAll();
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
