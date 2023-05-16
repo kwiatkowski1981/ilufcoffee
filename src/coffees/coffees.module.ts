a0ef86a0c7b0b96cd4399f9a74f0bbb4b307a646
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-16 11:51:45 +0200

    Class Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,24 +5,47 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import * as process from 'process';
 
+// example 1 Value based Provider
 // export class MockCoffeesService {} // 👈
+
+class ConfigService {} // 👈  👈  👈
+class DevelopmentConfigService {} // 👈  👈  👈
+class ProductionConfigService {} // 👈  👈  👈
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [CoffeesService],
+
+  // example 1 Value based Provider // 👈
   // providers: [
   //   {
   //     provide: CoffeesService,
   //     useValue: new MockCoffeesService(), // 👈
   //   },
   // ],
+
+  // example 2 Non-class-based Provider Tokens  👈 👈
+  // providers: [
+  //   CoffeesService,
+  //   {
+  //     provide: 'COFFEE_BRANDS',
+  //     useValue: ['buddy brew', 'nescafe'], // 👈 👈
+  //   },
+  // ],
+
+  // example 3 Class Providers  👈 👈  👈
   providers: [
     CoffeesService,
     {
-      provide: 'COFFEE_BRANDS',
-      useValue: ['buddy brew', 'nescafe'], // 👈
+      provide: ConfigService, // 👈 👈 👈
+      // 👈 👈 👈
+      useValue:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
     },
   ],
 
