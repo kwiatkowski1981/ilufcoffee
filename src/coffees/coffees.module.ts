697b63e1143bffb766b713817e1e9ce2fa4fa232
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-16 11:33:59 +0200

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,18 +6,26 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
-export class MockCoffeesService {} // 👈
+// export class MockCoffeesService {} // 👈
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [CoffeesService],
+  // providers: [
+  //   {
+  //     provide: CoffeesService,
+  //     useValue: new MockCoffeesService(), // 👈
+  //   },
+  // ],
   providers: [
+    CoffeesService,
     {
-      provide: CoffeesService,
-      useValue: new MockCoffeesService(), // 👈
+      provide: 'COFFEE_BRANDS',
+      useValue: ['buddy brew', 'nescafe'], // 👈
     },
   ],
+
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
