c422c9b5cc501bc36883b4a31391d2e6a3d1f0aa
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2023-05-19 14:22:30 +0200

    Bonus: Create Custom Param Decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -19,10 +19,11 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
-import { Request } from 'express';
+import { request, Request } from 'express';
 import { REQUEST } from '@nestjs/core';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from '../common/decorators/protocol.decorator';
 
 // @UsePipes(ValidationPipe)
 @Controller('coffees')
@@ -39,10 +40,15 @@ export class CoffeesController {
   @Public()
   @UsePipes(ValidationPipe)
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
     // const { limit, offset } = paginationQuery;
     // route to hit: localhost:3000/coffees?limit=10&offset=20
     // await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log(protocol);
+    // console.log(request.connection);
     return this.coffeesService.findAll(paginationQuery);
   }
 
